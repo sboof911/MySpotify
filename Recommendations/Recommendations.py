34a4c69414689e37bfb7d42b61a05bfece49ebe7
Sboof <56886152+sboof911@users.noreply.github.com>
2025-02-18 19:48:06 +0000

    Refactor Recommendations class for improved precision calculation and error handling; enhance data reading and mapping processes

--- Recommendations/Recommendations.py
+++ Recommendations/Recommendations.py
@@ -42,45 +42,53 @@ class Recommendations:
         hits = 0
         total = 0
 
-        for user in test_data["user_id"].unique():
-            test_tracks = set(test_data[test_data["user_id"] == user]["song_id"])
-            print(len(self._model.recommend(user, user_item_matrix[user], N=k)))
+        with tqdm(total=len(test_data["user_id"].unique()), desc="Calculating Precision") as pbar:
+            for user in test_data["user_id"].unique():
+                test_tracks = set(test_data[test_data["user_id"] == user]["song_id"])
 
-            recommended_tracks = [
-                track for track, _ in self._model.recommend(user, user_item_matrix[user], N=k)
-            ]
+                recommended_tracks = [
+                    track for track in self._model.recommend(user, user_item_matrix[user], N=k)[0]
+                ]
 
-            hits += len(set(recommended_tracks) & test_tracks)
-            total += len(test_tracks)
+                hits += len(set(recommended_tracks) & test_tracks)
+                total += len(test_tracks)
+                pbar.update(1)
 
         return hits / total * 100
 
     def fit(self):
         trainTriplits_pf = ParquetFile(os.path.join(self._Parquet_Data_dir, "train_triplets.parquet"))
-        Track_Data : pd.DataFrame = trainTriplits_pf.read().to_pandas()
-        if self._MapData == {}:
-            self.MapData(Track_Data)
-        Track_Data["user_id"] = Track_Data["user_id"].map(self._MapData["user_to_int"])
-        Track_Data["song_id"] = Track_Data["song_id"].map(self._MapData["song_to_int"])
-        train_data, test_data = train_test_split(Track_Data, test_size=0.2, random_state=42)
-        Track_Data = None
+        print("Reading Data")
+        try:
+            train_data : pd.DataFrame = trainTriplits_pf.read().to_pandas()
+            if self._MapData == {}:
+                self.MapData(train_data)
+            train_data["user_id"] = train_data["user_id"].map(self._MapData["user_to_int"])
+            train_data["song_id"] = train_data["song_id"].map(self._MapData["song_to_int"])
 
-        train_matrix = sp.coo_matrix((train_data["play_count"], (train_data["user_id"], train_data["song_id"]))).tocsr()
-        print("Fitting Model")
-        self._model.fit(train_matrix, show_progress=True)
-        precision = self.precision_at_k(test_data, train_matrix)
-        print(f"Precision at k: {precision:.2f}/100")
+            train_matrix = sp.coo_matrix((train_data["play_count"], (train_data["user_id"], train_data["song_id"]))).tocsr()
+            print("Fitting Model")
+            self._model.fit(train_matrix, show_progress=True)
+            test_data = train_data.sample(n=1000, random_state=42)
+            precision = self.precision_at_k(test_data, train_matrix)
+            print(f"Precision at k: {precision:.2f}/100")
+        except Exception as e:
+            train_data = None
+            raise
 
         return train_matrix
 
     def get_similar_tracks(self, user_id, num_of_tracks=10):
         if self._train_matrix is None:
             self._train_matrix = self.fit()
-        user_items = self._train_matrix[self._MapData["user_to_int"][user_id]]
 
-        scores = self._model.recommend(user_id, user_items, N=num_of_tracks)
+        mapped_user_id = self._MapData["user_to_int"][user_id]
+        user_items = self._train_matrix[mapped_user_id]
 
-        recommendations = pd.DataFrame(scores, columns=["song_id", "likelihood"])
+        scores = self._model.recommend(mapped_user_id, user_items, N=num_of_tracks)
+        recommendations = pd.DataFrame(columns=["song_id", "likelihood"])
+        recommendations["song_id"] = scores[0]
+        recommendations["likelihood"] = scores[1]
         recommendations["song_id"] = recommendations["song_id"].map(self._MapData["int_to_song"])
         return recommendations
 
